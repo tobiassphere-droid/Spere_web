acec65b37892b308a811f3972199e9c3a295ca2b
tobiassphere-droid <tobias.sphere@gmail.com>
2026-02-02 17:18:42 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite';
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                termin: resolve(__dirname, 'termin.html'),
-                impressum: resolve(__dirname, 'impressum.html'),
-                datenschutz: resolve(__dirname, 'datenschutz.html'),
+                main: 'index.html',
+                termin: 'termin.html',
+                impressum: 'impressum.html',
+                datenschutz: 'datenschutz.html',
             },
         },
     },
